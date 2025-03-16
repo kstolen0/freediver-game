fac02799c17a6b08adb30c596cc7c991cab883bd
Kristian Stolen <kstolen0@gmail.com>
2025-03-16 20:07:44 +0800

    add sound of veins while submerged

--- ui/breath_bar.gd
+++ ui/breath_bar.gd
@@ -6,7 +6,8 @@ var breath = MAX_BREATH / modifier
 var holding = false
 var sent_death_signal = false
 signal out_of_breath(toggle)
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var heart_beat: AudioStreamPlayer2D = $heart_beat
+@onready var veins: AudioStreamPlayer2D = $veins
 
 # Called when the node enters the scene tree for the first time.
 func _ready():	
@@ -16,28 +17,37 @@ func _ready():
 	$"../../player".holding_breath.connect(toggleOxygen)
 	$"../../player".recovering_breath.connect(toggleOxygen)
 	$"../../player".final_breath.connect(on_final_breath)
-	audio_stream_player_2d.play()
+	heart_beat.play()
+	veins.play()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if holding:
 		value -= 0.1
+		
 		if value <= 0:
 			if !sent_death_signal:
 				emit_signal("out_of_breath", true)
+				heart_beat.stop()
+				veins.stop()
 				sent_death_signal = !sent_death_signal
 	else:
 		if sent_death_signal:
 			emit_signal("out_of_breath", false)
+			heart_beat.play()
+			veins.play()
 			sent_death_signal = !sent_death_signal
 		recoverBreath()
+	
 	
 func toggleOxygen():
 	print("holding is ", holding)
 	if !holding:
-		audio_stream_player_2d.volume_db = 10
+		heart_beat.volume_db = 10
+		veins.volume_db = 20
 	else:
-		audio_stream_player_2d.volume_db = 1
+		heart_beat.volume_db = 2
+		veins.volume_db = 1
 	holding = !holding
 	
 func on_final_breath(state):
